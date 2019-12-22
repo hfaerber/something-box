477df00eab8616700784f2d80e91f08509337f66
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-22 12:57:34 -0700

    Create files

---
+++ src/DisplayContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DisplayContainer = () => {
+  return(
+
+  )
+}
+
+export default DisplayContainer;
---
+++ src/Form.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      character: '',
+      quote: '',
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState( {[event.target.name]: event.target.value} );
+  }
+
+  render() {
+    return(
+      <form>
+        <input type='text' value={this.state.character} name='character'
+          placeholder='Who said it?'
+          onChange={event => this.handleChange(event)} />
+        <input type='text' value={this.state.quote} name='quote'
+          placeholder='Quote'
+          onChange={event => this.handleChange(event)} />
+        <button> Submit </button>
+      </form>
+
+    )
+  }
+}
+
+export default Form;
