6b49e5c5995c6b6bcd0ffe4ed303f8245997be46
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-22 16:18:15 -0700

    Start adding images

--- src/Search.js
+++ src/Search.js
@@ -33,6 +33,24 @@ class Search extends Component {
           <h1>{this.props.isSearchDisplay ? `That's What *${this.state.query}* Said` : 'That\'s What She Said'}</h1>
           <button className={buttonStatus}>Back To All Results</button>
         </div>
+        <div>
+          <img src='' name="Michael:" className='image'/>
+          <img src='' name="Dwight:" className='image'/>
+          <img src='' name="Jim:" className='image'/>
+          <img src='' name="Pam:" className='image'/>
+          <img src='' name="Phyllis:" className='image'/>
+          <img src='' name="Creed:" className='image'/>
+          <img src='' name="Angela:" className='image'/>
+          <img src='' name="Kevin:" className='image'/>
+          <img src='' name="Meredith:" className='image'/>
+          <img src='' name="Tobuy:" className='image'/>
+          <img src='' name="Oscar:" className='image'/>
+          <img src='' name="Oscar:" className='image'/>
+          <img src='' name="Oscar:" className='image'/>
+
+
+
+        </div>
         <div className='searchInputs'>
           <input type="text" name='query' placeholder='Search Quote by Character'
             value={this.state.query} onChange={event => this.handleChange(event)} />
@@ -45,3 +63,6 @@ class Search extends Component {
 }
 
 export default Search;
+
+
+// use drop down menu or images instead of type-in search
