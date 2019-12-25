0382b4d4853bffd108d77425ef73d4afe50523ab
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-25 10:41:25 -0700

    Updates

---
+++ precache-manifest.147b74c8527bd4e828d9a87e30f4e9c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8fbd2b8fa11197a0dd798cc2fbcd3c",
+    "url": "/something-box/index.html"
+  },
+  {
+    "revision": "9c663005796ba565e909",
+    "url": "/something-box/static/css/main.9e99292d.chunk.css"
+  },
+  {
+    "revision": "6ee37a3215debe17c4dc",
+    "url": "/something-box/static/js/2.2f000b4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/something-box/static/js/2.2f000b4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c663005796ba565e909",
+    "url": "/something-box/static/js/main.c3735b8e.chunk.js"
+  },
+  {
+    "revision": "9b18c1f8716b0dda42e5",
+    "url": "/something-box/static/js/runtime-main.a81a0fa4.js"
+  }
+]);
